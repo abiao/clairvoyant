c2bf8bb65d5835abeec54a6b7835c7eaef55e2cf
abiao <abiao.chen@gmail.com>
2016-05-20 16:42:46 +0800

    Use UTF8 for default

--- src/com/egnore/clairvoyant/mail/Authentication.java
+++ src/com/egnore/clairvoyant/mail/Authentication.java
@@ -3,18 +3,20 @@ package com.egnore.clairvoyant.mail;
 import javax.mail.Authenticator;
 import javax.mail.PasswordAuthentication;
 
-public class Authentication  extends Authenticator {
-	  String username=null;  
-	  String password=null;  
-	  
-	  public Authentication(){  
-	  }  
-	  public Authentication(String username, String password) {  
-	  this.username = username;  
-	  this.password = password;  
-	  }  
-	  protected PasswordAuthentication getPasswordAuthentication(){
-	  PasswordAuthentication pa = new PasswordAuthentication(username, password);
-	  return pa;
-	  }  
+public class Authentication extends Authenticator {
+	String username = null;
+	String password = null;
+
+	public Authentication() {
+	}
+
+	public Authentication(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+
+	protected PasswordAuthentication getPasswordAuthentication() {
+		PasswordAuthentication pa = new PasswordAuthentication(username, password);
+		return pa;
+	}  
 }
